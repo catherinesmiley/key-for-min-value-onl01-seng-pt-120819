613d4f6f643e48f3c1963b99f579b52322e06ed1
Catherine Smiley <catherinesmiley@gmail.com>
2020-01-05 22:38:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,11 @@
 def key_for_min_value(hash)
   lowest_value = ""
   hash.collect do |key, value|
-    if value[0] > value[1]
-      value[1] = value[0]
+    if lowest_value == 0 || value < lowest_value
+      lowest_value = value
+      lowest_key = key
     else
-      value[0] = value[0]
+      value[0]
       lowest_value = key
     end
   end
