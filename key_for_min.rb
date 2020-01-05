008250898a0d4ab0b9ec37c724f0fe3ccd706232
Catherine Smiley <catherinesmiley@gmail.com>
2020-01-05 22:40:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(hash)
-  lowest_value = ""
+  lowest_value = 0
   lowest_key = ""
-  hash.collect do |key, value|
+  hash.each do |key, value|
     if lowest_value == 0 || value < lowest_value
       lowest_value = value
       lowest_key = key
